c4810125acf4b42efaf0cd66d5c52846734ab29f
David E. Wheeler <david@justatheory.com>
2010-01-17 22:22:41 -0800

    Lay out buzzer buttons.

--- Classes/BuzzerViewController.m
+++ Classes/BuzzerViewController.m
@@ -23,7 +23,32 @@
 
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
+	// XXX Fill in with the Buzzer's name. The OS automatically truncates as appropriate for the space.
+	self.title = NSLocalizedString(@"Theory", @"Buzzer view navigation title");
+	UIBarButtonItem *replyItem = [ [ UIBarButtonItem alloc ]
+									initWithBarButtonSystemItem: UIBarButtonSystemItemReply
+									target: self
+									action: nil
+									];
+	UIBarButtonItem *trashItem = [ [ UIBarButtonItem alloc ]
+									initWithBarButtonSystemItem: UIBarButtonSystemItemTrash
+									target: self
+									action: nil
+									];
+	UIBarButtonItem *refreshItem = [ [ UIBarButtonItem alloc ]
+									initWithBarButtonSystemItem: UIBarButtonSystemItemRefresh
+									target: self
+									action: nil
+									];
+	UIBarButtonItem *flexItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+
+	[self.navigationController setToolbarHidden:NO];
+	[self setToolbarItems: [NSArray arrayWithObjects: refreshItem, flexItem, trashItem, flexItem, replyItem, nil] animated: YES];
+
 	self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+	[replyItem release];
+	[flexItem release];
+	[refreshItem release];
 	[super viewDidLoad];
 }
 
