11bb3ab58bf871df1d0d248392ee61405b9a4e1f
David E. Wheeler <david@justatheory.com>
2010-01-19 23:06:53 -0800

    Add `TableViewCellBalloon`.

--- Classes/BuzzerViewController.m
+++ Classes/BuzzerViewController.m
@@ -77,5 +77,4 @@
     [super dealloc];
 }
 
-
 @end
---
+++ Classes/TableViewCellBalloon.m
@@ -0,0 +1,19 @@
+//
+//  TableViewCellBalloon.m
+//  Buzzalot
+//
+//  Created by David E. Wheeler on 1/19/10.
+//  Copyright 2010 Kineticode, Inc.. All rights reserved.
+//
+
+#import "TableViewCellBalloon.h"
+
+
+@implementation TableViewCellBalloon
+
+- (void)drawRect:(CGRect)rect {
+	UIImage* balloon = [[UIImage imageNamed:@"balloon.png"] stretchableImageWithLeftCapWidth:15  topCapHeight:15];
+	[balloon drawInRect: aRect];    
+}
+
+@end
